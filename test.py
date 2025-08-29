ef3ce68b5db9717efc6989d6c4461c6ad175cb57
Kihyuk Hong <kihyukh@umich.edu>
2025-08-29 21:14:55 +0900

    initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+import or_gym
+
+env = gym.make("or/TrafficControlEnv-v0", render_mode="human")
+env.reset()
+
+# Run a few simulation steps
+num_steps = 6000 # Increased the number of steps to better observe the effect of rendering frequency
+action = 1
+for i in range(num_steps):
+    # Take action 1 to turn on GR, then action 2 to trigger yellow light transition
+    if i % 600 == 0:
+        if action == 1:
+            action = 2
+        else:
+            action = 1
+
+    observation, reward, terminated, truncated, info = env.step(action)
+    if i % 10 == 0: # Render every 10 steps for efficiency
+        env.render()
